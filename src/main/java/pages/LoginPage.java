e38c4c8a0c98a8b9b7047acc5be0f349e9edb979
Dhruv3110 <109941217+Dhruv3110@users.noreply.github.com>
2026-02-04 01:57:23 +0530

    Implement Page Object Model with TestNG in Maven project

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,37 +1,76 @@
 package pages;
 
-import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utils.Log;
 
 public class LoginPage {
-	
+
 	private WebDriver driver;
-//	Locators of the Elements
-	private By userNameTextBox = By.id("Email");
-	private By passwordTextBox = By.id("Password");
-	private By loginButton = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
-	
+
+	// Elements Using @FindBy
+
+	@FindBy(id = "Email")
+	WebElement usernameTextbox;
+
+	@FindBy(id = "Password")
+	WebElement passwordTextbox;
+
+	@FindBy(xpath = "//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button")
+	WebElement loginButton;
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
-//	Actions on the Elements
+
+	// Actions Using @FindBy
+
 	public void enterUserName(String username) {
-		driver.findElement(userNameTextBox).clear();
-		driver.findElement(userNameTextBox).sendKeys(username);
+		Log.info("Entering Username: " + username);
+		usernameTextbox.clear();
+		usernameTextbox.sendKeys(username);
 	}
+
 	public void enterPassword(String password) {
-		driver.findElement(passwordTextBox).clear();
-		driver.findElement(passwordTextBox).sendKeys(password);
+		Log.info("Entering password: " + password);
+		passwordTextbox.clear();
+		passwordTextbox.sendKeys(password);
 	}
-	
+
 	public void clickLogin() {
-		driver.findElement(loginButton).click();
+		Log.info("Clicking Login Button...");
+		loginButton.click();
 	}
-	
-	
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
+
+	// Alternative Method (Using By Locators)
+
+	// Locators of the Elements
+//    private By userNameTextBox = By.id("Email");
+//    private By passwordTextBox = By.id("Password");
+//    private By loginBtn = By.xpath("//*[@id=\"main\"]/div/section/div/div[2]/div[1]/div/form/div[3]/button");
+
+	// Actions Using Locators
+
+//    public void enterUserNameUsingLocator(String username) {
+//        Log.info("Entering Username: " + username);
+//        driver.findElement(userNameTextBox).clear();
+//        driver.findElement(userNameTextBox).sendKeys(username);
+//    }
+
+//    public void enterPasswordUsingLocator(String password) {
+//        Log.info("Entering password: " + password);
+//        driver.findElement(passwordTextBox).clear();
+//        driver.findElement(passwordTextBox).sendKeys(password);
+//    }
+
+//    public void clickLoginUsingLocator() {
+//        Log.info("Clicking Login Button...");
+//        driver.findElement(loginBtn).click();
+//    }
 
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,25 +1,216 @@
 package tests;
 
+import java.io.IOException;
+
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExcelUtils;
+import utils.ExtentReportManager;
+import utils.Log;
 
-//	Only pure test scripts
+// ------------------------------------------------------
+// LoginTest Class (Only Pure Test Scripts)
+// Demonstrates Different TestNG Testing Approaches
+// ------------------------------------------------------
 public class LoginTest extends BaseTest {
-//	Create a test function
-	@Test
-	public void testValidLogin() {
+
+	// ======================================================
+	// Approach 1: Data Driven Testing Using Excel File
+	// (Excel DataProvider + Test Case Together)
+	// ======================================================
+	/*
+	@DataProvider(name = "LoginData")
+	public Object[][] getLoginData() throws IOException {
+
+		String filePath = System.getProperty("user.dir") + "/testData/TestData.xlsx";
+
+		ExcelUtils.loadExcel(filePath, "Sheet1");
+
+		int rowCount = ExcelUtils.getRowCount();
+		Object[][] data = new Object[rowCount - 1][2];
+
+		for (int i = 1; i < rowCount; i++) {
+			data[i - 1][0] = ExcelUtils.getCellData(i, 0); // username
+			data[i - 1][1] = ExcelUtils.getCellData(i, 1); // password
+		}
+
+		ExcelUtils.closeExcel();
+		return data;
+	}
+
+	@Test(dataProvider = "LoginData")
+	public void testValidLogin_ExcelData(String username, String password) {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test - " + username);
+
+		test.info("Navigating to URL..");
 		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials...");
+		test.info("Adding credentials..");
+
+		loginPage.enterUserName(username);
+		loginPage.enterPassword(password);
+
+		test.info("Clicking on Login Button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the Page: " + driver.getTitle());
+
+		Log.info("Verifying Page title...");
+		test.info("Verifying Page Title...");
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Login Successful.");
+	}
+	*/
+
+	// ======================================================
+	// Approach 2: Data Driven Testing Using Hardcoded Inline Data
+	// (DataProvider + Test Case Together)
+	// ======================================================
+	/*
+	@DataProvider(name = "LoginData2")
+	public Object[][] getData() {
+
+		return new Object[][] {
+			{ "user1", "pass1" },
+			{ "user2", "pass2" },
+			{ "user3", "pass3" }
+		};
+	}
+
+	@Test(dataProvider = "LoginData2")
+	public void testValidLogin_HardcodedData(String username, String password) {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test - " + username);
+
+		test.info("Navigating to URL..");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials...");
+		test.info("Adding credentials..");
+
+		loginPage.enterUserName(username);
+		loginPage.enterPassword(password);
+
+		test.info("Clicking on Login Button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the Page: " + driver.getTitle());
+
+		Log.info("Verifying Page title...");
+		test.info("Verifying Page Title...");
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Login Successful.");
+	}
+	*/
+
+	// ======================================================
+	// Approach 3: Parameterization Using TestNG XML (@Parameters)
+	// ======================================================
+	@Test
+	@Parameters({ "username", "password" })
+	public void testValidLogin_XMLParameters(String username, String password) {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test - " + username);
+
+		test.info("Navigating to URL..");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials...");
+		test.info("Adding credentials..");
+
+		loginPage.enterUserName(username);
+		loginPage.enterPassword(password);
+
+		test.info("Clicking on Login Button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the Page: " + driver.getTitle());
+
+		Log.info("Verifying Page title...");
+		test.info("Verifying Page Title...");
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Login Successful.");
+	}
+
+	// ======================================================
+	// Approach 4: Simple Login Test Without DataProvider (Static Data)
+	// ======================================================
+	/*
+	@Test
+	public void testValidLogin_StaticData() {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test - Static Data");
+
+		test.info("Navigating to URL..");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials...");
+		test.info("Adding credentials..");
+
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
-		loginPage.clickLogin();
-		System.out.println("Title of the Page: " + driver.getTitle());
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
-	}
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
 
+		test.info("Clicking on Login Button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the Page: " + driver.getTitle());
+
+		Log.info("Verifying Page title...");
+		test.info("Verifying Page Title...");
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Login Successful.");
+	}
+	*/
+
+	// ======================================================
+	// Approach 5: Negative Testing with Invalid Credentials
+	// ======================================================
+	/*
+	@Test
+	public void testLoginWithInvalidCredentials() {
+
+		Log.info("Starting invalid login test...");
+		test = ExtentReportManager.createTest("Login Test - Invalid Credentials");
+
+		test.info("Navigating to URL..");
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding invalid credentials...");
+		test.info("Adding invalid credentials..");
+
+		loginPage.enterUserName("wrongUser@gmail.com");
+		loginPage.enterPassword("wrongPassword");
+
+		test.info("Clicking on Login Button");
+		loginPage.clickLogin();
+
+		System.out.println("Title of the Page: " + driver.getTitle());
+
+		Log.info("Verifying Page title...");
+		test.info("Verifying Page Title...");
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+
+		test.pass("Invalid Login Test Completed.");
+	}
+	*/
 }
